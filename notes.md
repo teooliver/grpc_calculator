bb2414180d7b5f6e6b63500a6f37ebe193fd891b
Teo Oliver <teo.liz.oli@gmail.com>
2025-12-08 22:12:24 +0000

    chore: wip

--- notes.md
+++ notes.md
@@ -1,11 +1,31 @@
 # Commands:
 
 - Without reflection we need to send the `proto` flag
+```
 grpcurl -plaintext -proto ./proto/calculator.proto -d '{"a": 2, "b": 3}' '[::1]:50051' calculator.Calculator.Add
-
+```
 - After setting up reflection we don't need to add the `proto` flag as the server will know
 where to look by it self
+```
 grpcurl -plaintext -d '{"a": 2, "b": 3}' '[::1]:50051' calculator.Calculator.Add
+```
+
+- grpcui
+```
+grpcui -plaintext '[::1]:50051'
+```
+
+- Unauthenticated
+```
+grpcurl -emit-defaults -plaintext '[::1]:50051' calculator.Admin.GetRequestCount
+```
+
+- Authenticated
+```
+grpcurl -H "Authorization: Bearer some-secret-token"  -emit-defaults -plaintext '[::1]:50051' calculator.Admin.GetRequestCount
+```
+
+
 
 # Notes
 - Check the build.rs to understand how `tonic` uses `::compile_protos` and
@@ -14,3 +34,7 @@ grpcurl -plaintext -d '{"a": 2, "b": 3}' '[::1]:50051' calculator.Calculator.Add
 # Tools
 - grpcurl
 - grpcui
+
+
+Refs:
+https://www.youtube.com/watch?v=kerKXChDmsE
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use proto::admin_server::{Admin, AdminServer};
 use proto::calculator_server::{Calculator, CalculatorServer};
 use tonic::transport::Server;
 
@@ -8,8 +9,38 @@ mod proto {
         tonic::include_file_descriptor_set!("calculator_descriptor");
 }
 
+type State = std::sync::Arc<tokio::sync::RwLock<u64>>;
+
 #[derive(Debug, Default)]
-struct CalculatorService {}
+struct AdminService {
+    state: State,
+}
+
+#[tonic::async_trait]
+impl Admin for AdminService {
+    async fn get_request_count(
+        &self,
+        _request: tonic::Request<proto::GetCountRequest>,
+    ) -> Result<tonic::Response<proto::CounterResponse>, tonic::Status> {
+        let count = self.state.read().await;
+        let response = proto::CounterResponse { count: *count };
+
+        Ok(tonic::Response::new(response))
+    }
+}
+
+#[derive(Debug, Default)]
+struct CalculatorService {
+    state: State,
+}
+
+impl CalculatorService {
+    async fn increment_counter(&self) {
+        let mut count = self.state.write().await;
+        *count += 1;
+        println!("Request count: {}", *count);
+    }
+}
 
 #[tonic::async_trait]
 impl Calculator for CalculatorService {
@@ -17,6 +48,7 @@ impl Calculator for CalculatorService {
         &self,
         request: tonic::Request<proto::CalculationRequest>,
     ) -> Result<tonic::Response<proto::CalculationResponse>, tonic::Status> {
+        self.increment_counter().await;
         let input = request.get_ref();
 
         let response = proto::CalculationResponse {
@@ -25,12 +57,83 @@ impl Calculator for CalculatorService {
 
         Ok(tonic::Response::new(response))
     }
+
+    async fn subtract(
+        &self,
+        request: tonic::Request<proto::CalculationRequest>,
+    ) -> Result<tonic::Response<proto::CalculationResponse>, tonic::Status> {
+        self.increment_counter().await;
+
+        let input = request.get_ref();
+
+        let response = proto::CalculationResponse {
+            result: input.a - input.b,
+        };
+
+        Ok(tonic::Response::new(response))
+    }
+
+    async fn divide(
+        &self,
+        request: tonic::Request<proto::CalculationRequest>,
+    ) -> Result<tonic::Response<proto::CalculationResponse>, tonic::Status> {
+        self.increment_counter().await;
+
+        let input = request.get_ref();
+
+        if input.b == 0 {
+            return Err(tonic::Status::invalid_argument("cannot divide by zedo"));
+        };
+
+        let response = proto::CalculationResponse {
+            result: input.a / input.b,
+        };
+
+        Ok(tonic::Response::new(response))
+    }
+
+    async fn multiplicate(
+        &self,
+        request: tonic::Request<proto::CalculationRequest>,
+    ) -> Result<tonic::Response<proto::CalculationResponse>, tonic::Status> {
+        self.increment_counter().await;
+
+        let input = request.get_ref();
+
+        let response = proto::CalculationResponse {
+            result: input.a * input.b,
+        };
+
+        Ok(tonic::Response::new(response))
+    }
+}
+
+use tonic::metadata::MetadataValue;
+use tonic::{Request, Status};
+
+fn check_auth(req: Request<()>) -> Result<Request<()>, Status> {
+    let token: MetadataValue<_> = "Bearer some-secret-token".parse().unwrap();
+
+    match req.metadata().get("authorization") {
+        Some(t) if token == t => Ok(req),
+        _ => Err(Status::unauthenticated("No valid auth token")),
+    }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "[::1]:50051".parse()?;
-    let calc = CalculatorService::default();
+
+    let state = State::default();
+
+    let calc = CalculatorService {
+        state: state.clone(),
+    };
+
+    let admin = AdminService {
+        state: state.clone(),
+    };
+
     let service = tonic_reflection::server::Builder::configure()
         .register_encoded_file_descriptor_set(proto::FILE_DESCRIPTOR_SET)
         .build()?;
@@ -38,6 +141,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     Server::builder()
         .add_service(service)
         .add_service(CalculatorServer::new(calc))
+        .add_service(AdminServer::with_interceptor(admin, check_auth))
         .serve(addr)
         .await?;
 
